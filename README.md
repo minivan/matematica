f972b90a3f2237bbf378b396aea1363282effa6b
Maxian Nicu <maxiannicu@gmail.com>
2015-07-08 00:02:25 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Matematica Edu Soft
+#Matematica Edu Soft
+
+
+##Used in this project
+
+[JSDoc](https://github.com/jsdoc3/jsdoc)
+    
+    
---
+++ sample.js
@@ -0,0 +1,12 @@
+/**
+ * Created by nick on 7/7/15.
+ */
+
+/**
+ * Multiply number by itself
+ * @param number number to multiply
+ * @returns {number}
+ */
+function sqr(number){
+    return number*number;
+}
